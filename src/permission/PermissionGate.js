7d57631fd407c77b37098ad6b2c465cc71280dd8
lti-equipamiento <lti.equipamiento@gmail.com>
2022-11-09 17:16:13 -0300

    permissiongate

--- src/permission/PermissionGate.js
+++ src/permission/PermissionGate.js
@@ -1,6 +1,7 @@
 import { cloneElement } from "react";
 import { PERMISSIONS } from "./PermissionMaps";
 import jwt_decode from "jwt-decode";
+import { useAuth0 } from "@auth0/auth0-react";
 
 const hasPermission = ({ permissions, scopes }) => {
   const scopesMap = {};
@@ -17,18 +18,22 @@ export default function PermissionsGate({
   errorProps = null,
   scopes = [],
 }) {
-  const token = localStorage.getItem(
-    `@@auth0spajs@@::TU1HzLoMRbH5X4gA7FIFYji7hL88VXTi::https://test-api-agem.hasura.app/v1/graphql::openid profile email offline_access`
-  );
-  const role =
-    jwt_decode(token)["https://hasura.io/jwt/claims"]["x-hasura-custom"];
-  const permissions = PERMISSIONS[role];
-  const permissionGranted = hasPermission({ permissions, scopes });
+  const { isAuthenticated } = useAuth0();
 
-  if (!permissionGranted && !errorProps) return <RenderError />;
+  if (isAuthenticated) {
+    const token = localStorage.getItem(
+      `@@auth0spajs@@::TU1HzLoMRbH5X4gA7FIFYji7hL88VXTi::https://test-api-agem.hasura.app/v1/graphql::openid profile email offline_access`
+    );
+    const role =
+      jwt_decode(token)["https://hasura.io/jwt/claims"]["x-hasura-custom"];
+    const permissions = PERMISSIONS[role];
+    const permissionGranted = hasPermission({ permissions, scopes });
 
-  if (!permissionGranted && errorProps)
-    return cloneElement(children, { ...errorProps });
+    if (!permissionGranted && !errorProps) return <RenderError />;
+
+    if (!permissionGranted && errorProps)
+      return cloneElement(children, { ...errorProps });
+  }
 
   return <>{children}</>;
 }
