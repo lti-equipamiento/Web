adaa175d8159315901b96a53fecd294c1db1ea12
Agustin Thevenet <agustinthevenet@gmail.com>
2022-11-09 16:50:01 -0300

    hide sidebar rol

--- src/layout/NavigationItems.jsx
+++ src/layout/NavigationItems.jsx
@@ -9,51 +9,63 @@ import ConfirmationNumberIcon from "@mui/icons-material/ConfirmationNumber";
 import MedicalInformationIcon from "@mui/icons-material/MedicalInformation";
 import * as React from "react";
 import { NavLink } from "react-router-dom";
+import PermissionsGate from "../permission/PermissionGate";
+import { SCOPES } from "../permission/PermissionMaps";
 
 const navLinkStyle = { textDecoration: "none", color: "black" };
 
 export const mainListItems = (
   <React.Fragment>
-    <NavLink to="/dashboard" style={navLinkStyle}>
-      <ListItemButton>
-        <ListItemIcon>
-          <DashboardIcon />
-        </ListItemIcon>
-        <ListItemText primary="Dashboard" />
-      </ListItemButton>
-    </NavLink>
-    <NavLink to="/usuario" style={navLinkStyle}>
-      <ListItemButton>
-        <ListItemIcon>
-          <PeopleIcon />
-        </ListItemIcon>
-        <ListItemText primary="Usuarios" />
-      </ListItemButton>
-    </NavLink>
-    <NavLink to="/equipo" style={navLinkStyle}>
-      <ListItemButton>
-        <ListItemIcon>
-          <MedicalInformationIcon />
-        </ListItemIcon>
-        <ListItemText primary="Equipos" />
-      </ListItemButton>
-    </NavLink>
-    <NavLink to="/ticket" style={navLinkStyle}>
-      <ListItemButton>
-        <ListItemIcon>
-          <ConfirmationNumberIcon />
-        </ListItemIcon>
-        <ListItemText primary="Tickets" />
-      </ListItemButton>
-    </NavLink>
-    <NavLink to="/mantenimiento" style={navLinkStyle}>
-      <ListItemButton>
-        <ListItemIcon>
-          <HandymanIcon />
-        </ListItemIcon>
-        <ListItemText primary="Mantenimientos" />
-      </ListItemButton>
-    </NavLink>
+    <PermissionsGate scopes={[SCOPES.canViewDashboard]}>
+      <NavLink to="/dashboard" style={navLinkStyle}>
+        <ListItemButton>
+          <ListItemIcon>
+            <DashboardIcon />
+          </ListItemIcon>
+          <ListItemText primary="Dashboard" />
+        </ListItemButton>
+      </NavLink>
+    </PermissionsGate>
+    <PermissionsGate scopes={[SCOPES.canViewUsuario]}>
+      <NavLink to="/usuario" style={navLinkStyle}>
+        <ListItemButton>
+          <ListItemIcon>
+            <PeopleIcon />
+          </ListItemIcon>
+          <ListItemText primary="Usuarios" />
+        </ListItemButton>
+      </NavLink>
+    </PermissionsGate>
+    <PermissionsGate scopes={[SCOPES.canViewEquipo]}>
+      <NavLink to="/equipo" style={navLinkStyle}>
+        <ListItemButton>
+          <ListItemIcon>
+            <MedicalInformationIcon />
+          </ListItemIcon>
+          <ListItemText primary="Equipos" />
+        </ListItemButton>
+      </NavLink>
+    </PermissionsGate>
+    <PermissionsGate scopes={[SCOPES.canViewTicket]}>
+      <NavLink to="/ticket" style={navLinkStyle}>
+        <ListItemButton>
+          <ListItemIcon>
+            <ConfirmationNumberIcon />
+          </ListItemIcon>
+          <ListItemText primary="Tickets" />
+        </ListItemButton>
+      </NavLink>
+    </PermissionsGate>
+    <PermissionsGate scopes={[SCOPES.canViewMantenimiento]}>
+      <NavLink to="/mantenimiento" style={navLinkStyle}>
+        <ListItemButton>
+          <ListItemIcon>
+            <HandymanIcon />
+          </ListItemIcon>
+          <ListItemText primary="Mantenimientos" />
+        </ListItemButton>
+      </NavLink>
+    </PermissionsGate>
   </React.Fragment>
 );
 
